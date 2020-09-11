ce123b1fe40383556a827539c4074288883aa83a
Hannah Hudson <hannah.hch@gmail.com>
2020-09-11 16:51:29 -0600

    Added request for lat and long and got markers working

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import './Map.css'
 
-const LeafletMap = (props) =>{
 
+const LeafletMap = (props) => {
   const position = { lon: -104.991531, lat: 39.742043 }
-
   let markers = props.dataFromParent.map(data => {
     return (
       <div key={data._id}>
-        <Marker position={position}>
+        <Marker position={{ lon: data.lon, lat: data.lat}}>
           <Popup>
             <div>{data.sender}</div>
             <div>{data.location}</div>
@@ -23,12 +22,12 @@ const LeafletMap = (props) =>{
   
   return (
     <div className="map">
-      <Map center={position} zoom={13}>
+      <Map center={position} zoom={2}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="http://a.tile.stamen.com/toner/{z}/{x}/{y}.png"
+          // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         {markers}
       </Map>
     </div>
--- src/services/postcard-service.js
+++ src/services/postcard-service.js
@@ -1,19 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import Loader from '../components/Loader/Loader';
 import LeafletMap from '../components/Map/Map';
+import { GoogleProvider } from 'leaflet-geosearch';
 
 const PostcardService = () => {
   const [postcards, setPostcards] = useState(null);
+  const provider = new GoogleProvider({
+    params: {
+      key: 'AIzaSyDFxdIE3cbGB7T64Lnj5RRy6T6nRia2mww',
+    },
+  });
 
   async function fetchData (endpoint) {
     try {
       const response = await fetch(endpoint);
-      const json = await response.json()
+      const json = await response.json();
+      getSearchableStrings(json);
+      await getLongAndLat(json)
       setPostcards(json);
     }
     catch(error) {
       console.error(error)
     }
+  }
+
+  function getLongAndLat(data) {
+    return Promise.all(
+      data.map(el => {
+        return provider.search({
+          query: el.searchString
+        })
+      })
+    ).then(providerValues => providerValues.forEach((value, index) => {
+          const dataItem = data[index];
+          if (value[0] !== undefined) {
+            dataItem.lon = value[0].x;
+            dataItem.lat = value[0].y;
+          } else {
+            dataItem.lon = null;
+            dataItem.lat = null;
+          }
+      })
+    )
+  }
+
+  const getSearchableStrings = (data) => {
+    data.forEach(el => {
+      el.searchString = `${el.country} ${el.city} ${el.state}`;
+    });
+    return data;
   }
 
   useEffect(() => {
